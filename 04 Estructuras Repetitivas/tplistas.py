f578204279b5b729bcac90a8a103df61e307aa36
Bautista Munoz <baumu2005@gmail.com>
2025-04-24 10:23:14 -0300

    tp listas

---
+++ 04 Estructuras Repetitivas/tplistas.py
@@ -0,0 +1,54 @@
+#PUNTO 1
+lista = range(1,101)
+
+#PUNTO 2
+mi_lista = ["pizza", "fútbol", "musica", "dormir", "series"]
+print(mi_lista[-2])
+
+#PUNTO 3
+lista_vacia = [ ]
+lista_vacia.append("hola")
+lista_vacia.append("cuando")
+lista_vacia.append("donde")
+#print (lista_vacia)
+
+#PUNTO 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+#print (animales)
+
+#PUNTO 5
+#Crea una lista llamada numero con esos elementos, busca el numero mas grande de la lista con el max, que seria 22 y elimina ese numero usando remove, y luego imprime la lista modificada
+
+#PUNTO 6
+lista_corta = range(10,31,5)
+#print (lista_corta[0])
+#print (lista_corta[1])
+
+#PUNTO 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "honda fit"
+autos[2] = "sw4"
+#print (autos)
+
+#PUNTO 8
+dobles = [ ]
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print (dobles)
+
+#PUNTO 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],
+["agua"]]
+compras[2].append("jugo")
+
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#PUNTO 10
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
+
